b160666e50c0d4ac03e8239d1f944c9499b1dba9
greg vandenberg <gvandenb@gregs-MacBook-Pro.local>
2024-02-05 23:38:08 -0800

    update

--- src/main/java/com/radionow/stream/service/StationService.java
+++ src/main/java/com/radionow/stream/service/StationService.java
@@ -1,5 +1,11 @@
 package com.radionow.stream.service;
 
+import java.util.List;
+
+import com.radionow.stream.model.Station;
+
 public interface StationService {
+
+	public List<Station> getStationByCallsign(String callsign);
 
 }
--- src/main/java/com/radionow/stream/service/StationServiceImpl.java
+++ src/main/java/com/radionow/stream/service/StationServiceImpl.java
@@ -1,5 +1,21 @@
 package com.radionow.stream.service;
 
-public class StationServiceImpl  implements StationService {
-	// TODO implement me
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.radionow.stream.dao.StationRepository;
+import com.radionow.stream.model.Station;
+
+@Service
+public class StationServiceImpl implements StationService {
+
+    @Autowired
+    private StationRepository stationRepository;
+
+    @Override
+    public List<Station> getStationByCallsign(String callsign) {
+        return stationRepository.findByCallsignContaining(callsign);
+    }
 }
